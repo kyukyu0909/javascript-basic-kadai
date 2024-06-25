11285ec26df9bfb96290c851b5a729c680e0ba1f
kyukyu0909 <miitan.dance@gmail.com>
2024-06-25 13:44:14 +0900

    変数に修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,9 @@
-// 四則演算
-console.log(68 / (1.7 * 1.7));
-
 // 変数の宣言・値の代入
-let userNumber
+let weight = 68;
+let height = 1.7;
+
+// 四則演算
+let BMI = (weight / (height * height));
+console.log(BMI);
+
+
